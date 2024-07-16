9bc6ad72c019ecaa8f2d895e4f11a9428ae1c04d
KknanxX <72762570+KknanxX@users.noreply.github.com>
2024-07-16 07:43:36 -0700

    Initial Commit

---
+++ CompanySearchApp/CompanySearchAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  CompanySearchAppApp.swift
+//  CompanySearchApp
+//
+//  Created by KKNANXX on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CompanySearchAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
